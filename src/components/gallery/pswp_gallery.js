d6d3c2d65815dd92959544be5c6ebe708ff71623
wass <wassim.filali@gmail.com>
2024-03-23 18:12:02 +0100

    dynamic import

--- src/components/gallery/pswp_gallery.js
+++ src/components/gallery/pswp_gallery.js
@@ -1,10 +1,22 @@
-import PhotoSwipeLightbox from 'photoswipe/lightbox';
-import 'photoswipe/style.css';
+async function initPhotoSwipe() {
+  // Dynamically import the PhotoSwipeLightbox module and CSS
+  const { default: PhotoSwipeLightbox } = await import('photoswipe/lightbox');
+  await import('photoswipe/style.css');
 
-const lightbox = new PhotoSwipeLightbox({
-    gallery: '#my-gallery',
-    children: 'a',
-    pswpModule: () => import('photoswipe')
+  // Initialize PhotoSwipeLightbox
+  const lightbox = new PhotoSwipeLightbox({
+      gallery: '#my-gallery',
+      children: 'a',
+      pswpModule: () => import('photoswipe') // This remains the same, dynamically importing the main module
   });
-  
-lightbox.init();
+
+  // Initiate the lightbox
+  lightbox.init();
+}
+
+// Call initPhotoSwipe on page load
+if (document.readyState === 'loading') {  // Loading hasn't finished yet
+  document.addEventListener('DOMContentLoaded', initPhotoSwipe);
+} else {  // `DOMContentLoaded` has already fired
+  initPhotoSwipe();
+}
